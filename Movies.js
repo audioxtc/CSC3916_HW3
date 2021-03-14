287b3cc5f586d1e3cb11789d55b45278a42a38ce
audioxtc <jeremy.2.gage@ucdenver.edu>
2021-03-13 23:18:40 -0700

    added connection to Movies.js

--- Movies.js
+++ Movies.js
@@ -1,8 +1,16 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
 
 var actorlist = [];
 
+try {
+    mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+        console.log("connected"));
+}catch (error) {
+    console.log("could not connect");
+}
+mongoose.set('useCreateIndex', true);
 
 var actorSchema = new Schema({
     actorName: String,
